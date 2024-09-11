e0e0be46b21057578a38bf48b5f1d862ea4a5dc6
25069 <25069@YAPINFANG>
2024-09-11 18:50:42 +0800

    添加项目文件。

---
+++ ApolloUsage/Controllers/GetStringController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApolloUsage.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GetStringController(IConfiguration configuration) : ControllerBase
+{
+    private readonly IConfiguration configuration = configuration;
+
+    [HttpGet]
+    public async Task<ActionResult<string>> GetConfigurationAsync()
+    {
+        var res= configuration.GetSection("RabbitMQ").Value;
+        return Ok(res);
+    }
+}
